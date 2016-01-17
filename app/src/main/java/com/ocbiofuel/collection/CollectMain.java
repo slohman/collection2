a96ff80cfb42e3a6e1bf04409d810c94d5171e8a
slohman <scott@Scotts-MacBook-Air-3.local>
2016-01-17 11:01:22 -0800

    Initial Commit

---
+++ app/src/main/java/com/ocbiofuel/collection/CollectMain.java
@@ -0,0 +1,20 @@
+package com.ocbiofuel.collection;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CollectMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collectmain);
+    }
+
+
+    }
+
+
+
